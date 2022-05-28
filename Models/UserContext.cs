0cf690cd542d16274d1a366803569058520bf6a1
mi-touch <timileyinadefare@yahoo.com>
2022-05-28 23:37:01 +0100

    UserContext defined

---
+++ Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserApiTest.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Books { get; set; }
+    }
+}
