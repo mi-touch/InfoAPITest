bbd7da8323ad87cb9719e69e0dab314c3497c893
Timileyin Adefare <61581959+mi-touch@users.noreply.github.com>
2024-11-21 23:10:51 +0100

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace UserApiTest.Models
+public class User
 {
-    public class User
-    {           
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
 }
+
