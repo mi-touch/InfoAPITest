ef28f78214a3e8f34758842985e5da8848352656
Timileyin Adefare <61581959+mi-touch@users.noreply.github.com>
2024-11-21 23:15:55 +0100

    Update IUserRepository.cs

--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -1,17 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using UserApiTest.Models;
-
-namespace UserApiTest.Repository
+/// <summary>
+/// Defines methods for managing User entities in the repository.
+/// </summary>
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-        Task<IEnumerable<User>> Get();
-        Task<User> Get(int id);
-        Task<User> Create(User user);
-        Task Update(User user);
-        Task Delete(int id);
-    }
+    /// <summary>
+    /// Retrieves all users.
+    /// </summary>
+    /// <returns>A collection of users.</returns>
+    Task<IEnumerable<User>> Get();
+
+    /// <summary>
+    /// Retrieves a specific user by ID.
+    /// </summary>
+    /// <param name="id">The ID of the user to retrieve.</param>
+    /// <returns>The user with the specified ID.</returns>
+    Task<User> Get(int id);
+
+    /// <summary>
+    /// Creates a new user in the repository.
+    /// </summary>
+    /// <param name="user">The user to create.</param>
+    /// <returns>The created user.</returns>
+    Task<User> Create(User user);
+
+    /// <summary>
+    /// Updates an existing user.
+    /// </summary>
+    /// <param name="user">The user with updated details.</param>
+    Task Update(User user);
+
+    /// <summary>
+    /// Deletes a user by ID.
+    /// </summary>
+    /// <param name="id">The ID of the user to delete.</param>
+    Task Delete(int id);
 }
